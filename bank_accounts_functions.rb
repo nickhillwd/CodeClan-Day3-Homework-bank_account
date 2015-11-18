d4b17fa4bd439e2a60dbd3ec6d0d0f973d734565
nickhillwd <nickhillwd@gmail.com>
2015-11-18 16:41:54 +0000

    initial commit. Homework issued

---
+++ bank_accounts_functions.rb
@@ -0,0 +1,48 @@
+ACCOUNTS = [
+  {
+    holder_name: "Jay",
+    amount: 55,
+    type: "business"
+  },
+  {
+    holder_name: "Rick",
+    amount: 1,
+    type: "personal"
+  },
+  {
+    holder_name: "Keith",
+    amount: 500,
+    type: "business"
+  },
+  {
+    holder_name: "Valerie",
+    amount: 100,
+    type: "personal"
+  },
+  {
+    holder_name: "Michael",
+    amount: 5,
+    type: "personal"
+  },
+  {
+    holder_name: "Kate",
+    amount: 2000,
+    type: "business"
+  },
+  {
+    holder_name: "Tony",
+    amount: 150,
+    type: "personal"
+  },
+  {
+    holder_name: "Sandy",
+    amount: 4500,
+    type: "business"
+  },
+]
+
+def number_of_bank_accounts()
+  ACCOUNTS.length
+end
+
+
---
+++ bank_accounts_runner.rb
@@ -0,0 +1,34 @@
+require_relative('./bank_accounts_functions')
+
+##number of bank accounts
+bank_accounts_count = number_of_bank_accounts()
+puts "number of accounts #{bank_accounts_count}"
+puts "\n"
+
+###first account owner
+# first_bank_owner = first_bank_account_holder()
+# puts "first bank account owner is #{first_bank_owner}"
+# puts "\n"
+
+###print to screen name of all bank account owners
+# puts "name of bank acccount owners"
+# print_owner_names()
+# puts "\n"
+
+###total cash in bank
+# total_cash = total_cash_in_bank()
+# puts "total in bank #{total_cash}"
+# puts "\n"
+
+###last account owner
+
+###average bank account value
+
+###total cash in business accounts
+
+###holder of largest bank account
+
+###holder of largest personal account
+
+###any other functionality you want to add.
+
